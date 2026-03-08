d122fe7a81837feba1578341525f025b23fc6de4
Viktoria Karpenko <ocenkachprao2003@gmail.com>
2026-03-08 12:10:32 +0200

    seventh JS-homework

---
+++ js/task-4.js
@@ -0,0 +1,18 @@
+const registerForm = document.querySelector('.login-form');
+registerForm.addEventListener('submit', handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.currentTarget;
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+  if (email === '' || password === '') {
+    return alert('All form fields must be filled in');
+  }
+  const formData = {
+    email,
+    password,
+  };
+
+  console.log(formData);
+  form.reset();
+}
